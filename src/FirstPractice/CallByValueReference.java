c0c5c2edd4856b361a932bb81efb4f6017d40524
zelldingct <zell9931@gmail.com>
2022-07-11 20:34:26 +0900

    220711FirstCommit

---
+++ src/FirstPractice/CallByValueReference.java
@@ -0,0 +1,29 @@
+package FirstPractice;
+
+public class CallByValueReference {
+	int m = 2;
+
+	public static void main(String[] args) {
+//		int i =1;
+//		System.out.println("i="	+  i);
+//		change1(i);
+//		System.out.println("i="	+  i);
+	 
+		CallByValueReference cbvr = new CallByValueReference();
+		System.out.println("cb1=" + cbvr.m);
+		
+		change2(cbvr);
+		System.out.println("cb2=" + cbvr.m);
+	 
+	 
+	}
+	
+	public static void change1( int x) {
+		x=100;	
+	}
+	
+	public static void change2( CallByValueReference cb) {
+		cb.m = 100;
+	}
+
+}
